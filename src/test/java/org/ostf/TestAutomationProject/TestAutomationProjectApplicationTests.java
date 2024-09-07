adc0876a3d7900af9c4d12fc4daebd88d01dfabd
Mitch <mitchscox@gmail.com>
2024-09-07 15:47:33 -0500

    NTTAP-15: removing unused import

--- src/test/java/org/ostf/TestAutomationProject/TestAutomationProjectApplicationTests.java
+++ src/test/java/org/ostf/TestAutomationProject/TestAutomationProjectApplicationTests.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 
 import java.util.Comparator;

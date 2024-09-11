ac31cfe158bb8af0f45459fd115b17f67d89dc9f
Mitch <mitchscox@gmail.com>
2024-09-10 22:37:51 -0500

    NTTAP-21: Enhancing logging in all requests in objects service request, remove postconstruct logging

--- src/main/java/org/ostf/TestAutomationProject/ObjectsService.java
+++ src/main/java/org/ostf/TestAutomationProject/ObjectsService.java
@@ -6,40 +6,56 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+//import javax.annotation.PostConstruct;
+
 @Service
 public class ObjectsService {
 
     Logger logger = LogManager.getLogger();
 
-    // Inject the value from application.properties
     @Value("${objects.api.endpoint}")
     private String endpoint;
 
     private final BaseService baseService;
 
-    // Constructor to initialize BaseService
+    // Constructor that injects BaseService
     public ObjectsService(BaseService baseService) {
         this.baseService = baseService;
-        logger.info("Object Service Created ");
     }
 
+    // Use @PostConstruct to log after the endpoint has been set by Spring
+    /* @PostConstruct
+    public void init() {
+        logger.info("Object Service Created with endpoint: {}", endpoint);
+    }
+    */
+    // Method to fetch all objects
     public Response getAllObjects() {
+        logger.info("Fetching all objects from endpoint: {}", endpoint);
         return baseService.getRequest(endpoint);
     }
 
+    // Method to fetch an object by ID
     public Response getObjectById(String id) {
+        logger.info("Fetching object by ID: {} from endpoint: {}", id, endpoint);
         return baseService.getRequest(endpoint + "/" + id);
     }
 
+    // Method to create an object
     public Response createObject(Object body) {
+        logger.info("Creating object at endpoint: {}", endpoint);
         return baseService.postRequest(endpoint, body);
     }
 
+    // Method to update an object
     public Response updateObject(String id, Object body) {
+        logger.info("Updating object with ID: {} at endpoint: {}", id, endpoint);
         return baseService.putRequest(endpoint + "/" + id, body);
     }
 
+    // Method to delete an object
     public Response deleteObject(String id) {
+        logger.info("Deleting object with ID: {} from endpoint: {}", id, endpoint);
         return baseService.deleteRequest(endpoint + "/" + id);
     }
 }

7705317224548ba1ec72f3d73d48c9c81d7d0ad6
Mitch <mitchscox@gmail.com>
2024-09-12 14:35:13 -0500

    NTTAP-22: Initial code modifications for remote running

--- src/main/java/org/ostf/TestAutomationProject/TestAutomationProjectApplication.java
+++ src/main/java/org/ostf/TestAutomationProject/TestAutomationProjectApplication.java
@@ -1,13 +1,13 @@
 package org.ostf.TestAutomationProject;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { ManagementWebSecurityAutoConfiguration.class })
 public class TestAutomationProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestAutomationProjectApplication.class, args);
 	}
-
 }
